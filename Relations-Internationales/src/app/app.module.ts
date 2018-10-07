abb3532d82dbce09e6eade9a044c9fa9f413c3ff
Virgil Bontempi <virgilbontempi@hotmail.fr>
2018-10-07 15:41:28 +0200

    Initiate routing process

--- Relations-Internationales/src/app/app.module.ts
+++ Relations-Internationales/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent }, // localhost/4200
+  { path: 'login', component: LoginComponent }, // localhost/4200/login
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
