ea1aa34f73a788c20747653db11abe38ce4cba2a
Virgil Bontempi <virgilbontempi@hotmail.fr>
2018-10-10 10:42:32 +0200

    Fill classes Person, Student and Contact

--- Relations-Internationales/src/app/models/contact.ts
+++ Relations-Internationales/src/app/models/contact.ts
@@ -1,2 +1,41 @@
 export class Contact {
+
+    private idContact: string;
+    private idStudent: string;
+    private emailAddress: string;
+    private firstName: string;
+    private lastName: string;
+    private phoneNumber: string;
+    private affiliation: string;
+    private description: string;
+
+    constructor(idContact: string, idStudent: string, emailAddress: string, firstName: string, lastName: string,
+        phoneNumber: string, affiliation: string, description: string) {
+        this.idContact = idContact;
+        this.idStudent = idStudent;
+        this.emailAddress = emailAddress;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.affiliation = affiliation;
+        this.description = description;
+    }
+
+    public getIdContact(): string { return this.idContact; }
+    public getIdStudent(): string { return this.idStudent; }
+    public getEmailAddress(): string { return this.emailAddress; }
+    public getFirstName(): string { return this.firstName; }
+    public getLastName(): string { return this.lastName; }
+    public getPhoneNumber(): string { return this.phoneNumber; }
+    public getAffiliation(): string { return this.affiliation; }
+    public getDescription(): string { return this.description; }
+
+    public setIdContact(value: string): void { this.idContact = value; }
+    public setIdStudent(value: string): void { this.idStudent = value; }
+    public setEmailAddress(value: string): void { this.emailAddress = value; }
+    public setFirstName(value: string): void { this.firstName = value; }
+    public setLastName(value: string): void { this.lastName = value; }
+    public setPhoneNumber(value: string): void { this.phoneNumber = value; }
+    public setAffiliation(value: string): void { this.affiliation = value; }
+    public setDescription(value: string): void { this.description = value; }
 }
--- Relations-Internationales/src/app/models/student.ts
+++ Relations-Internationales/src/app/models/student.ts
@@ -1,4 +1,29 @@
 import { Person } from './person';
+import { Contact } from './contact';
 
 export class Student extends Person {
+
+    private university: string;
+    private isEntrant: boolean;
+    private isArchived: boolean;
+    private contacts: Contact[];
+
+    constructor(idPerson: string, emailAddress: string, firstName: string, lastName: string,
+        birthDate: Date, lastConnection: string, phoneNumber: string,
+        university: string, isEntrant: boolean, isArchived: boolean, contacts: Contact[]) {
+        super(idPerson, emailAddress, firstName, lastName, birthDate, lastConnection, phoneNumber);
+        this.university = university;
+        this.isEntrant = isEntrant;
+        this.isArchived = isArchived;
+    }
+
+    public getUniversity(): string { return this.university; }
+    public getIsEntrant(): boolean { return this.isEntrant; }
+    public getIsArchived(): boolean { return this.isArchived; }
+    public getContacts(): Contact[] { return this.contacts; }
+
+    public setUniversity(value: string): void { this.university = value; }
+    public setIsEntrant(value: boolean): void { this.isEntrant = value; }
+    public setIsArchived(value: boolean): void { this.isArchived = value; }
+    public setContacts(value: Contact[]): void { this.contacts = value; }
 }
