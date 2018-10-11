8240fc60347023eed0d9cd61aff4e06db0830ec7
Virgil Bontempi <virgilbontempi@hotmail.fr>
2018-10-11 17:53:33 +0200

    Add buttons and display archived students function

--- Relations-Internationales/src/app/pages/home/administrator-side/administrator-side.component.ts
+++ Relations-Internationales/src/app/pages/home/administrator-side/administrator-side.component.ts
@@ -10,20 +10,56 @@ import { MatTableDataSource, MatPaginator } from '@angular/material';
 })
 export class AdministratorSideComponent implements OnInit {
 
-  private students: Student[];
+  private archivedStudents: Student[];
+  private nonArchivedStudents: Student[];
   private dataSource: MatTableDataSource<Student>;
   private displayedColumns: string[];
+  private areDisplayArchived: boolean;
 
   constructor(private simulator: SimulatorService) { }
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngOnInit() {
-    this.students = this.simulator.getStudents();
+    this.areDisplayArchived = false;
+    this.archivedStudents = [];
+    this.nonArchivedStudents = [];
+    this.initStudentsLists();
 
+    this.setDataSource();
     this.displayedColumns = ['Signal', 'Name', 'University', 'Last connection', 'Entrant/Leaving', 'OpenInNew'];
-    this.dataSource = new MatTableDataSource<Student>(this.students);
     this.dataSource.paginator = this.paginator;
   }
 
+  initStudentsLists(): void {
+    this.simulator.getStudents().forEach(student => {
+      if (student.getIsArchived()) {
+        this.archivedStudents.push(student);
+      } else {
+        this.nonArchivedStudents.push(student);
+      }
+    });
+  }
+
+  archiveStudents(): void {
+    console.log('Archive function clicked.');
+  }
+
+  displayArchivedStudents() {
+    console.log('Display archived students function clicked.');
+    this.areDisplayArchived = !this.areDisplayArchived;
+    this.setDataSource();
+  }
+
+  addStudent() {
+    console.log('Add student function clicked.');
+  }
+
+  setDataSource(): void {
+    if (this.areDisplayArchived) {
+      this.dataSource = new MatTableDataSource<Student>(this.archivedStudents);
+    } else {
+      this.dataSource = new MatTableDataSource<Student>(this.nonArchivedStudents);
+    }
+  }
 }
