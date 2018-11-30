2b39ed1f4523f09315b04deb5e6c315adf0f8363
Virgil Bontempi <virgilbontempi@hotmail.fr>
2018-11-30 09:12:46 +0100

    Check add contact form validity

--- Relations-Internationales/src/app/components/add-element-dialog/add-contact-dialog/add-contact-dialog.component.ts
+++ Relations-Internationales/src/app/components/add-element-dialog/add-contact-dialog/add-contact-dialog.component.ts
@@ -10,24 +10,74 @@ import { Contact } from 'src/app/models/contact';
 })
 export class AddContactDialogComponent implements OnInit {
 
-  private firstName: string;
-  private lastName: string;
-  private student: Student;
-  private emailAddress: string;
-  private phoneNumber: string;
-  private affiliation: string;
-  private description: string;
+  firstName: string;
+  lastName: string;
+  student: Student;
+  emailAddress: string;
+  phoneNumber: string;
+  affiliation: string;
+  description: string;
+
+  isFirstNameValid: boolean;
+  isLastNameValid: boolean;
+  isEmailAddressValid: boolean;
+  isPhoneNumberValid: boolean;
+  isAffiliationValid: boolean;
+  isFormValid: boolean;
 
   constructor(private dialogRef: MatDialogRef<AddContactDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public injectedStudent: Student) { }
 
   ngOnInit() {
     this.student = this.injectedStudent;
+    this.isFormValid = true;
   }
 
   createContact(): void {
-    this.dialogRef.close(new Contact(null, this.student, this.emailAddress, this.firstName, this.lastName,
-      this.phoneNumber, this.affiliation, this.description));
+    if (this.checkForm()) {
+      this.dialogRef.close(new Contact(null, this.student, this.emailAddress, this.firstName, this.lastName,
+        this.phoneNumber, this.affiliation, this.description));
+    }
+  }
+
+  checkForm(): boolean {
+
+    if (this.firstName && this.firstName.length > 0) {
+      this.isFirstNameValid = true;
+    } else {
+      this.isFirstNameValid = false;
+      this.isFormValid = false;
+    }
+
+    if (this.lastName && this.lastName.length > 0) {
+      this.isLastNameValid = true;
+    } else {
+      this.isLastNameValid = false;
+      this.isFormValid = false;
+    }
+
+    if (this.emailAddress && this.emailAddress.length > 0) {
+      this.isEmailAddressValid = true;
+    } else {
+      this.isEmailAddressValid = false;
+      this.isFormValid = false;
+    }
+
+    if (this.phoneNumber && this.phoneNumber.length < 10) {
+      this.isPhoneNumberValid = true;
+    } else {
+      this.isPhoneNumberValid = false;
+      this.isFormValid = false;
+    }
+
+    if (this.affiliation && this.affiliation.length > 0) {
+      this.isAffiliationValid = true;
+    } else {
+      this.isAffiliationValid = false;
+      this.isFormValid = false;
+    }
+
+    return this.isFormValid;
   }
 
 }
